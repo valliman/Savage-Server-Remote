7ded4d2ce52f40a9395cc7ddafe5d810d4e0de58
Bernhard Fritz <bernhard.fritz@students.htlinn.ac.at>
2012-07-07 19:38:44 +0200

    asdf

---
+++ desktop/src/main/java/Login.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bernhard
+ * Date: 07.07.12
+ * Time: 13:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Login {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JList list1;
+    private JButton connectButton;
+    private JButton addToFavoritesButton;
+}
